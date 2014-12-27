aec8e01fbb459bd7fee9e5475d23504ec79f89f4
Eric Kuhn <erickuhn19@gmail.com>
2014-12-26 19:11:55 -0500

    Added edit and delete to posts. Also fixed the All Post link in header

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,27 @@ class PostsController < ApplicationController
 		@post = Post.new
 	end
 
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update 
+		@post = Post.find(params[:id])
+
+		if @post.update(params[:post].permit(:title, :body))
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to posts_path
+	end
+
 	def create
 		@post = Post.new(post_params)
 
